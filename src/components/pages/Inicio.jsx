1658713140034bb19e9bf9f362d7121843cad021
Melipilla <melipilla@duoc.acad>
2025-10-10 14:20:31 -0300

    cambios

--- src/components/pages/Inicio.jsx
+++ src/components/pages/Inicio.jsx
@@ -8,7 +8,7 @@ export default function Inicio() {
 
   return (
     <>
-    <TopBar/>
+    <TopBar/> 
     <div id="products">
         <Product
             code="1"
@@ -22,6 +22,36 @@ export default function Inicio() {
             name="Te matcha"
             description="no se ni q wea es"
             price="$4.990" />
+        <Product
+            code="3"
+            image="https://casaverde.mx/cdn/shop/files/te-matcha-propiedades-y-usos_01.jpg?v=1717786284&width=493"
+            name="Te matcha"
+            description="no se ni q wea es"
+            price="$4.990" />
+        <Product
+            code="3"
+            image="https://casaverde.mx/cdn/shop/files/te-matcha-propiedades-y-usos_01.jpg?v=1717786284&width=493"
+            name="Te matcha"
+            description="no se ni q wea es"
+            price="$4.990" />
+        <Product
+            code="1"
+            image="https://i5.walmartimages.cl/asr/55c8b34e-637a-4c7e-91a6-cf5edf74031e.8e578ce5b098c035cadfb2d5c4d6a09b.jpeg?odnHeight=612&odnWidth=612&odnBg=FFFFFF"
+            name="labubu"
+            description="esta wea es pa wekos"
+            price="$89.990" />
+        <Product
+            code="1"
+            image="https://i5.walmartimages.cl/asr/55c8b34e-637a-4c7e-91a6-cf5edf74031e.8e578ce5b098c035cadfb2d5c4d6a09b.jpeg?odnHeight=612&odnWidth=612&odnBg=FFFFFF"
+            name="labubu"
+            description="esta wea es pa wekos"
+            price="$89.990" />
+        <Product
+            code="1"
+            image="https://i5.walmartimages.cl/asr/55c8b34e-637a-4c7e-91a6-cf5edf74031e.8e578ce5b098c035cadfb2d5c4d6a09b.jpeg?odnHeight=612&odnWidth=612&odnBg=FFFFFF"
+            name="labubu"
+            description="esta wea es pa wekos"
+            price="$89.990" />
     </div>  
     </>
   )
